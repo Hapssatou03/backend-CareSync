5a2300085a30a0227c465351965298a740b5a0c0
Hapssatou <hapssatou.sy@dadi-tech.net>
2025-12-29 16:24:17 +0100

    chore: initialize Spring Boot project structure

---
+++ src/main/java/com/caresync/CaresyncBackendApplication.java
@@ -0,0 +1,13 @@
+package com.caresync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaresyncBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaresyncBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caresync/CaresyncBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caresync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaresyncBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
